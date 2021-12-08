1dd8f748623bb57c2a7b4419965a2441fe9723c8
Uğur Karataş <ukaratas@rocketmail.com>
2021-12-08 15:54:10 +0300

    database upgrade

--- src/bbt.service.notification-profile/Model/SourceDisplayType.cs
+++ src/bbt.service.notification-profile/Model/SourceDisplayType.cs
@@ -4,9 +4,9 @@ using System.ComponentModel.DataAnnotations;
 
 public enum SourceDisplayType
 {
-   NotDisplay,
-   Display,
-   DisplaySetSwitch,
-   DisplayAndSetSwitchParameters,
-   DisplayAndSetSwitchParametersChannelsInfo,
+    NotDisplay = 1,
+    Display = 2,
+    DisplaySetSwitch = 3,
+    DisplayAndSetSwitchParameters = 4,
+    DisplayAndSetSwitchParametersChannelsInfo = 5,
 }
