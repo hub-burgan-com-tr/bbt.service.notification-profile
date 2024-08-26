a76772b98c68343472435666a57a12dd3e571a98
Huseyin Tureoglu <htureoglu@burgan.com.tr>
2024-08-26 13:49:15 +0300

    Phone convert

---
+++ src/bbt.service.notification-profile/Helper/ConvertHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace Notification.Profile.Helper
+{
+    public static class ConvertHelper
+    {
+        public static int ToInt(string value)
+        {
+            try
+            {
+                return Convert.ToInt32(value);
+            }
+            catch 
+            {
+               return 0;
+            }
+        }
+    }
+}
